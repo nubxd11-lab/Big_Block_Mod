ed3f88b32dc6cdf57aaebe78bc537a25da940ec9
nubxd11-lab <nubxd11@gmail.com>
2026-07-18 22:44:30 +0800

    Update PlayerEvolutions.java

--- src/main/java/com/danklin/playerevolutions/PlayerEvolutions.java
+++ src/main/java/com/danklin/playerevolutions/PlayerEvolutions.java
@@ -1,5 +1,8 @@
 package com.danklin.playerevolutions;
 
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -12,6 +15,12 @@ import org.apache.logging.log4j.Logger;
 public class PlayerEvolutions {
     private static final Logger LOGGER = LogManager.getLogger();
     public static final String MOD_ID = "playerevolutions";
+        public static final ItemGroup TAB = new ItemGroup("playerevolutions_tab") {
+            @Override
+            public ItemStack createIcon() {
+                return new ItemStack(Items.DIAMOND);
+            }
+        };
 
     public PlayerEvolutions() {
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
